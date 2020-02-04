c016df5978d52a9a0c14348ad247b6a59a6dfd1f
Yasminvb <yasminvanbussel@gmail.com>
2020-02-04 10:55:34 +0100

    deleted item

--- Function/wall-of-fame.js
+++ Function/wall-of-fame.js
@@ -1,25 +1,24 @@
-et name = 'Miloud'
-let skill = 'Growth Hacking'
-function wallOfFame(){
-    return `
+let name = "Miloud";
+let skill = "Growth Hacking";
+function sum1() {
+  return `
                                     WALL OF FAME
         _|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
         ___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|__
         _|___|___|___|___|___|  ${name} : ${skill} |___|___|___|___|___|
         ___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|__
         _|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
-    `
+    `;
 }
-// const miloud = wallOfFame()
-// console.log(miloud)
-// name = 'Johan'
-// skill = 'Fixing Ubuntu'
-// const johan = wallOfFame()
-// console.log(johan)
-// name = 'Danny'
-// skill = 'Product Owning'
-// const danny = wallOfFame()
-// console.log(danny)
+const miloud = wallOfFame();
+console.log(miloud);
+name = "Johan";
+skill = "Fixing Ubuntu";
+const johan = wallOfFame();
+console.log(johan);
+name = "Danny";
+skill = "Product Owning";
+const danny = wallOfFame();
+console.log(danny);
 
 // Change the wall of fame function so it depends on its arguments
-const sum1 = wallOfFame
